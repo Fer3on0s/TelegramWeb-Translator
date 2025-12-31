71b74267a807e47f012643e80f6e654f0aee0ea2
𝐅𝐞𝐫𝟑𝐨𝐧™ <farao5922@gmail.com>
2025-12-31 14:14:33 +0200

    Create Telegram-Translator.meta.js

---
+++ Telegram-Translator.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         🌐 Telegram Translator Pro — Matrix Universal v7.0
+// @namespace    Fer3on_Mod
+// @version      7.0
+// @description  The ultimate translation tool for Telegram Web. Translates incoming/outgoing messages with a Matrix-style UI. Protected Version.
+// @author       Fer3on_Mod
+// @match        https://web.telegram.org/k/*
+// @match        https://web.telegram.org/a/*
+// @license      MIT
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @connect      translate.googleapis.com
+// @connect      clients5.google.com
+// @downloadURL  https://github.com/Fer3on0s/TelegramWeb-Translator/raw/refs/heads/main/Telegram-Translator.user.js
+// @updateURL    https://github.com/Fer3on0s/TelegramWeb-Translator/raw/refs/heads/main/Telegram-Translator.user.js
+// ==/UserScript==
+
+/* ========================================================================
+   🌐 Telegram Translator Pro — Developed by Fer3on_Mod
+   COPYRIGHT PROTECTED - DO NOT MODIFY
+   ======================================================================== */
